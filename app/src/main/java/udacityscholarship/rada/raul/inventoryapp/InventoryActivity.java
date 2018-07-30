0623b0b8f0a273b71dbc8d84f04d2231fa0c35d3
raulrada <raul.rada@gmail.com>
2018-07-30 20:10:07 +0300

    Implemented basic loader manager functionality in InventoryActivity

--- app/src/main/java/udacityscholarship/rada/raul/inventoryapp/InventoryActivity.java
+++ app/src/main/java/udacityscholarship/rada/raul/inventoryapp/InventoryActivity.java
@@ -7,6 +7,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.preference.ListPreference;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,7 +27,7 @@ import udacityscholarship.rada.raul.inventoryapp.data.ProductContract;
  * Main activity of the app. Shows a list of available products and permits navigation to other
  * activities within the app
  */
-public class InventoryActivity extends AppCompatActivity {
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     /**
      * Number of dummy products which should be inserted in the database every time the user selects
@@ -51,6 +55,9 @@ public class InventoryActivity extends AppCompatActivity {
      */
     private static final int PRODUCT_MAX_QUANTITY = 10;
 
+    /** Identifier for the product data loader */
+    private static final int PRODUCT_LOADER = 0;
+
     /** Adapter for the ListView */
     ProductCursorAdapter productCursorAdapter;
 
@@ -62,12 +69,6 @@ public class InventoryActivity extends AppCompatActivity {
         //set activity label programatically, instead of within AndroidManifest, in order to have
         //a different app label displayed on user's device vs. the label of the launcher activity
         getSupportActionBar().setTitle(R.string.inventory_activity_label);
-    }
-
-    /**
-     * Displays all entries in the products table within the inventory database
-     */
-    private void displayDatabaseInfo() {
 
         // find the ListView displaying data about products and the text view which will be
         // displayed when the ListView is empty
@@ -75,27 +76,15 @@ public class InventoryActivity extends AppCompatActivity {
         TextView emptyTextView = (TextView) findViewById(R.id.empty_text_view);
         productsListView.setEmptyView(emptyTextView);
 
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query.
-        String[] projection = {
-                ProductContract.ProductEntry._ID,
-                ProductContract.ProductEntry.COLUMN_PRODUCT_NAME,
-                ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE,
-                ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY,
-                ProductContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER,
-                ProductContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER
-        };
-
-        // Perform a query on the provider using the ContentResolver.
-        // Use the {@link ProductContract.ProductEntry#CONTENT_URI} to access product data
-        Cursor cursor = getContentResolver().query(ProductContract.ProductEntry.CONTENT_URI,
-                projection, null, null, null);
-
         // Setup a CursorAdapter to create list items for each row of product data in the cursor.
-        productCursorAdapter = new ProductCursorAdapter(this, cursor);
+        // There is no product data yet (until the loader finishes) so pass in null for the Cursor.
+        productCursorAdapter = new ProductCursorAdapter(this, null);
 
         // Attach the adapter to the ListView
         productsListView.setAdapter(productCursorAdapter);
+
+        // Kick off the loader
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
     }
 
     /**
@@ -135,16 +124,6 @@ public class InventoryActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), getString(R.string.product_save_successful),
                     Toast.LENGTH_SHORT).show();
         }
-    }
-
-    /**
-     * Override the default onStart() method of the activity in order to make sure
-     * tableContentTextView is updated when returning from other activities.
-     */
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
     }
 
     /**
@@ -195,9 +174,40 @@ public class InventoryActivity extends AppCompatActivity {
                     insertProduct(productName, productPrice, productQuantity, productSupplier,
                             PRODUCT_SUPPLIER_PHONE_NUMBER);
                 }
-                displayDatabaseInfo();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Define a projection that specifies which columns from the database which are relevant.
+        String[] projection = {
+                ProductContract.ProductEntry._ID,
+                ProductContract.ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE,
+                ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY
+        };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,                // Parent activity context
+                ProductContract.ProductEntry.CONTENT_URI,   // Provider content URI to query
+                projection,                                 // Columns to include in the resulting Cursor
+                null,                               // No selection clause
+                null,                            // No selection arguments
+                null);                             // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update the {@link ProductCursorAdapter} with this new cursor containing updated
+        // product data.
+        productCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        productCursorAdapter.swapCursor(null);
     }
 }
